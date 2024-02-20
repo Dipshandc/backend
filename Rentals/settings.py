21f66a2275c35343e794d6269b4c40127e6504d4
Dipshandc <dipshandcnew@gmail.com>
2024-02-20 21:06:23 +0545

    changed smto

--- Rentals/settings.py
+++ Rentals/settings.py
@@ -127,10 +127,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-server.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = 'carrentalconfrim@gmail.com'  
-EMAIL_HOST_PASSWORD = 'carrental2@confirm'  
+EMAIL_HOST_USER = ''  
+EMAIL_HOST_PASSWORD = ''  
 DEFAULT_FROM_EMAIL = 'from@carrental.com'
 EMAIL_USE_TLS = False
